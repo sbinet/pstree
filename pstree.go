e973934f2c81901fe91ee98a137769cc069384f3
Sebastien Binet <binet@cern.ch>
2023-03-21 16:15:56 +0100

    pstree: add JSON serialization
    Signed-off-by: Sebastien Binet <binet@cern.ch>

--- pstree.go
+++ pstree.go
@@ -27,11 +27,11 @@ func New() (*Tree, error) {
 		if err != nil {
 			return nil, fmt.Errorf("could not scan %s: %w", dir, err)
 		}
-		if proc.Stat.Pid == 0 {
+		if proc.Stat.PID == 0 {
 			// process vanished since Glob.
 			continue
 		}
-		procs[proc.Stat.Pid] = proc
+		procs[proc.Stat.PID] = proc
 	}
 
 	for pid, proc := range procs {
@@ -45,7 +45,7 @@ func New() (*Tree, error) {
 			)
 		}
 		parent.Children = append(parent.Children, pid)
-		procs[parent.Stat.Pid] = parent
+		procs[parent.Stat.PID] = parent
 	}
 
 	for pid, proc := range procs {
@@ -71,30 +71,30 @@ const (
 // ProcessStat contains process information.
 // see: http://man7.org/linux/man-pages/man5/proc.5.html
 type ProcessStat struct {
-	Pid       int    // process ID
-	Comm      string // filename of the executable in parentheses
-	State     byte   // process state
-	Ppid      int    // pid of the parent process
-	Pgrp      int    // process group ID of the process
-	Session   int    // session ID of the process
-	Tty       int    // controlling terminal of the process
-	Tpgid     int    // ID of foreground process group
-	Flags     uint32 // kernel flags word of the process
-	Minflt    uint64 // number of minor faults the process has made which have not required loading a memory page from disk
-	Cminflt   uint64 // number of minor faults the process's waited-for children have made
-	Majflt    uint64 // number of major faults the process has made which have required loading a memory page from disk
-	Cmajflt   uint64 // number of major faults the process's waited-for children have made
-	Utime     uint64 // user time in clock ticks
-	Stime     uint64 // system time in clock ticks
-	Cutime    int64  // children user time in clock ticks
-	Cstime    int64  // children system time in clock ticks
-	Priority  int64  // priority
-	Nice      int64  // the nice value
-	Nthreads  int64  // number of threads in this process
-	Itrealval int64  // time in jiffies before next SIGALRM is sent to the process due to an interval timer
-	Starttime int64  // time the process started after system boot in clock ticks
-	Vsize     uint64 // virtual memory size in bytes
-	Rss       int64  // resident set size: number of pages the process has in real memory
+	PID       int    `json:"pid"`       // process ID
+	Comm      string `json:"comm"`      // filename of the executable in parentheses
+	State     byte   `json:"state"`     // process state
+	Ppid      int    `json:"ppid"`      // pid of the parent process
+	Pgrp      int    `json:"pgrp"`      // process group ID of the process
+	Session   int    `json:"session"`   // session ID of the process
+	TTY       int    `json:"tty"`       // controlling terminal of the process
+	Tpgid     int    `json:"tpgid"`     // ID of foreground process group
+	Flags     uint32 `json:"flags"`     // kernel flags word of the process
+	Minflt    uint64 `json:"minflt"`    // number of minor faults the process has made which have not required loading a memory page from disk
+	Cminflt   uint64 `json:"cminflt"`   // number of minor faults the process's waited-for children have made
+	Majflt    uint64 `json:"majflt"`    // number of major faults the process has made which have required loading a memory page from disk
+	Cmajflt   uint64 `json:"cmajflt"`   // number of major faults the process's waited-for children have made
+	Utime     uint64 `json:"utime"`     // user time in clock ticks
+	Stime     uint64 `json:"stime"`     // system time in clock ticks
+	Cutime    int64  `json:"cutime"`    // children user time in clock ticks
+	Cstime    int64  `json:"cstime"`    // children system time in clock ticks
+	Priority  int64  `json:"priority"`  // priority
+	Nice      int64  `json:"nice"`      // the nice value
+	Nthreads  int64  `json:"nthreads"`  // number of threads in this process
+	Itrealval int64  `json:"itrealval"` // time in jiffies before next SIGALRM is sent to the process due to an interval timer
+	Starttime int64  `json:"starttime"` // time the process started after system boot in clock ticks
+	Vsize     uint64 `json:"vsize"`     // virtual memory size in bytes
+	RSS       int64  `json:"rss"`       // resident set size: number of pages the process has in real memory
 }
 
 func scan(dir string) (Process, error) {
@@ -118,7 +118,7 @@ func scan(dir string) (Process, error) {
 	}
 
 	var proc Process
-	proc.Stat.Pid, err = strconv.Atoi(info[0])
+	proc.Stat.PID, err = strconv.Atoi(info[0])
 	if err != nil {
 		return Process{}, fmt.Errorf("%s: invalid pid format %q: %w", stat, info[0], err)
 	}
@@ -128,7 +128,7 @@ func scan(dir string) (Process, error) {
 		info[2], statfmt,
 		&proc.Stat.State,
 		&proc.Stat.Ppid, &proc.Stat.Pgrp, &proc.Stat.Session,
-		&proc.Stat.Tty, &proc.Stat.Tpgid, &proc.Stat.Flags,
+		&proc.Stat.TTY, &proc.Stat.Tpgid, &proc.Stat.Flags,
 		&proc.Stat.Minflt, &proc.Stat.Cminflt, &proc.Stat.Majflt, &proc.Stat.Cmajflt,
 		&proc.Stat.Utime, &proc.Stat.Stime,
 		&proc.Stat.Cutime, &proc.Stat.Cstime,
@@ -136,7 +136,7 @@ func scan(dir string) (Process, error) {
 		&proc.Stat.Nice,
 		&proc.Stat.Nthreads,
 		&proc.Stat.Itrealval, &proc.Stat.Starttime,
-		&proc.Stat.Vsize, &proc.Stat.Rss,
+		&proc.Stat.Vsize, &proc.Stat.RSS,
 	)
 	if err != nil {
 		return proc, fmt.Errorf("could not parse file %s: %w", stat, err)
@@ -149,12 +149,12 @@ func scan(dir string) (Process, error) {
 
 // Tree is a tree of processes.
 type Tree struct {
-	Procs map[int]Process
+	Procs map[int]Process `json:"procs"`
 }
 
 // Process stores information about a UNIX process.
 type Process struct {
-	Name     string
-	Stat     ProcessStat
-	Children []int
+	Name     string      `json:"name"`
+	Stat     ProcessStat `json:"stat"`
+	Children []int       `json:"children"`
 }
